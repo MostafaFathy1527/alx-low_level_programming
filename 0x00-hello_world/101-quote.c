386c2004d1d84962d9cdea03ef5c976605f93284
Your Name <you@example.com>
2023-02-16 16:46:18 -0800

    2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+/**
+ * main - Printing ouput to the standard error
+ *
+ * Return: 1 (Success)
+ * Using fprintf
+ */
+
+int main(void)
+{
+fprintf(stderr,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
